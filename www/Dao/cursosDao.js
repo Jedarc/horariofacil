12e7aa5e7cb345a955fcd087f66c806c5f99392a
Jedarc <jedaiasrc7@hotmail.com>
2015-06-07 20:10:18 -0300

    Update Ajax function

--- www/Dao/cursosDao.js
+++ www/Dao/cursosDao.js
@@ -1,49 +1,30 @@
 function cursosDao() {
 	this.getCursos = function() {
 
-		var cursos = [];
+		var items = [];
+		ajax();
+		function ajax() {
+			alert("chamou ajax!")
+			$.getJSON(
+					"http://horariofacil.azurewebsites.net/Mobile/ObterCursos",
+					function(data) {
 
-		var resultado = new Object();
+						$.each(data, function(key, val) {
+							items.push({
+								"Value" : val.Codigo,
+								"Name" : val.Nome
+							});
+						});
 
-		/*
-		$.ajax({
-			url : "http://localhost:10642/Mobile/ObterCursos",
-			type : "GET",
-			dataType : "jsonp",
-			success : function(dados) {
-				resultado = dados;
-				alert("json sucess")
-			},
-			error : function(xhr, ajaxOptions, error) {
-				alert("erro json: " + xhr.responseText)
-			}
-		});
-		*/
+					});
 
-		function posAjax() {
-			alert("pos ajax " + resultado.length)
 		}
 
-		cursos.push({
-			"Value" : "Anáse de Sistemas",
-			"Name" : "Anáse de Sistemas"
-		});
-
-		cursos.push({
-			"Value" : "Ciência da Computação",
-			"Name" : "Ciência da Computação"
-		});
-		
-		cursos.push({
-			"Value" : "Redes",
-			"Name" : "Redes"
-		});
-		
-		cursos.push({
-			"Value" : "Nutrição",
-			"Name" : "Nutrição"
-		});
-
-		return cursos;
+		if (items > 0) {
+			alert(items[0].Name);
+			return items;
+		} else {
+			ajax();
+		}
 	}
 }
