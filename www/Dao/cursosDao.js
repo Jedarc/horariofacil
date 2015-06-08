e65c84db465789a8c8d6c2160dc07d3071c1b20a
Jedarc <jedaiasrc7@hotmail.com>
2015-06-07 21:40:17 -0300

    Ajax funcionando

--- www/Dao/cursosDao.js
+++ www/Dao/cursosDao.js
@@ -1,30 +1,25 @@
 function cursosDao() {
 	this.getCursos = function() {
 
-		var items = [];
-		ajax();
-		function ajax() {
-			alert("chamou ajax!")
-			$.getJSON(
-					"http://horariofacil.azurewebsites.net/Mobile/ObterCursos",
-					function(data) {
+		$.ajaxSetup({
+			async : false
+		});
 
-						$.each(data, function(key, val) {
-							items.push({
-								"Value" : val.Codigo,
-								"Name" : val.Nome
-							});
+		var items = [];
+		
+		$.getJSON("http://horariofacil.azurewebsites.net/Mobile/ObterCursos",
+				function(data) {
+
+					$.each(data, function(key, val) {
+						items.push({
+							"Value" : val.Codigo,
+							"Name" : val.Nome
 						});
 
 					});
 
-		}
-
-		if (items > 0) {
-			alert(items[0].Name);
-			return items;
-		} else {
-			ajax();
-		}
+				});
+		
+		return items;
 	}
 }
--- www/controllers/cursosController.js
+++ www/controllers/cursosController.js
@@ -1,27 +1,24 @@
 // Objeto controller para guardar as funções necessárias.
 var controller = {
 
-    // Obtendo o curso selecionado pelo usuário.
-    setNomeCurso: function (curso) {
-        if (curso != null) {
-            location.href = "semestres.html?" + curso;
-        } else {
-            alert("Ops. Ocorreu um erro, vamos tentar de novo!");
-            location.href = "cursos.html";
-        }
-    },
+	// Obtendo o curso selecionado pelo usuário.
+	setNomeCurso : function(curso) {
+		if (curso != null) {
+			location.href = "semestres.html?" + curso;
+		} else {
+			alert("Ops. Ocorreu um erro, vamos tentar de novo!");
+			location.href = "cursos.html";
+		}
+	},
 
-    // Obtendo todos os cursos para serem exibidas na tela.
-    getCursos: function () {
-        
-    	var cursos = new cursosDao();
-    	resultado = cursos.getCursos();
-    	
-    	if(resultado.length > 0){
-    		return resultado;
-    	} else {
-    		return null;
-    	}
-    }
+	// Obtendo todos os cursos para serem exibidas na tela.
+	getCursos : function() {
+
+		var cursos = new cursosDao();
+		resultado = cursos.getCursos();
+
+		return resultado;
+
+	}
 
 }
